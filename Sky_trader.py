b910601f1ec30d7bbbd42c49348ac0d2a874b2a4
Believer438 <believershemu20@gmail.com>
2025-06-25 12:51:00 +0200

    first commit

--- Sky_trader.py
+++ Sky_trader.py
@@ -6,6 +6,7 @@ import pandas as pd
 from datetime import datetime
 from binance.client import Client
 from binance.enums import *
+from binance.enums import ORDER_TYPE_TAKE_PROFIT_MARKET, ORDER_TYPE_STOP_MARKET
 from ta.trend import EMAIndicator
 from dotenv import load_dotenv
 import requests
